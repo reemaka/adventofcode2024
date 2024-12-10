62170052adc9671cecea7bd883cb5ce1bd0f997b
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2024-12-09 19:40:26 -0500

    Day 9 part 2.

--- day9/soln.py
+++ day9/soln.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 
 def read_file():
     f = open(sys.argv[1] if len(sys.argv) > 1 else "ex.txt", "r")
@@ -13,7 +14,6 @@ line = list(map(int, list(line)))
 def part_1():
     file_id_to_len = line[::2]
     l = 0
-    r = len(line) - 1
     checksum = 0
     checksum_i = 0
     id_num = 0
@@ -46,4 +46,68 @@ def part_1():
     
     print(checksum)
 
+class Node:
+    def __init__(self, file_id, block_size, is_free):
+        self.file_id = file_id # 0 if this is a free space
+        self.block_size = block_size 
+        self.is_free = is_free
+    def __repr__(self):
+        return str(self.file_id) + " " + str(self.block_size) + " " + str(self.is_free)
+    def __str__(self):
+        return str(self.file_id) + " " + str(self.block_size) + " " + str(self.is_free)
+
+
+def debug(nodes):
+    res = ""
+    for node in nodes:
+        for i in range(node.block_size):
+            if node.is_free:
+                res += "."
+            else:
+                res += str(node.file_id)
+    print(res)
+
+def part_2():
+    nodes = []
+    file_index = 0
+    for i, elem in enumerate(line):
+        if i % 2 == 0:
+            nodes.append(Node(file_index, elem, False))
+            file_index += 1
+        else:
+            nodes.append(Node(0, elem, True))
+    
+    nodes_copy = copy.deepcopy(nodes)
+    for i, file_node in reversed(list(enumerate(nodes_copy))):
+        if file_node.is_free:
+            continue
+
+        for j, free_node in enumerate(nodes):
+            if not free_node.is_free:
+                continue
+            if file_node.block_size <= free_node.block_size:
+                index_to_remove = -1
+                for k, f in reversed(list(enumerate(nodes))):
+                    if not f.is_free and f.file_id == file_node.file_id:
+                        index_to_remove = k
+                        break
+                if index_to_remove <= j:
+                    break
+                f = nodes.pop(index_to_remove)
+                nodes.insert(index_to_remove, Node(0, file_node.block_size, True))
+                nodes.insert(j, f)
+                free_node.block_size -= file_node.block_size
+                if free_node.block_size == 0:
+                    nodes.pop(j + 1)
+                break
+
+    checksum = 0
+    checksum_i = 0
+    for node in nodes:
+        for i in range(node.block_size):
+            checksum += node.file_id * checksum_i
+            checksum_i += 1
+    print(checksum)
+
 part_1()
+part_2() 
